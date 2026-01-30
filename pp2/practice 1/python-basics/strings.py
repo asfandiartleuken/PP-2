7e4fa8e6c99790685d12ca81b3f0971aa3267e69
Asfandiyar <asfandiartleuken8@gmail.com>
2026-01-30 18:17:45 +0500

    string examplse

---
+++ pp2/practice 1/python-basics/strings.py
@@ -0,0 +1,32 @@
+#1
+print("Hello")
+print('Hello')
+#2
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#3
+a = "Hello"
+print(a)
+#4
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#5
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+#6
+a = "Hello, World!"
+print(a[1])
+#7
+for x in "banana":
+  print(x)
+#8
+a = "Hello, World!"
+print(len(a))
+
