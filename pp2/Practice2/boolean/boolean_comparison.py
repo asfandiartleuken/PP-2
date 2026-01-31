ce7575a6a5dc95a2cd056f0c053a10ba5a7f1c13
Asfandiyar <asfandiartleuken8@gmail.com>
2026-01-31 22:23:08 +0500

    boolean comprasiom

---
+++ pp2/Practice2/boolean/boolean_comparison.py
@@ -0,0 +1,24 @@
+#1
+a = 10
+b = 5
+
+print(a > b)   # True
+print(a == b)  # False
+#2
+x = int(input())
+
+print(x >= 0)
+#3
+age = 18
+has_id = True
+
+print(age >= 18 and has_id)
+
+#4
+score = 45
+
+print(score >= 50 or score == 45)
+#5
+is_raining = False
+
+print(not is_raining)
