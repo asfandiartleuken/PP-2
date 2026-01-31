0a8307e479ff6944153932ab580359371f2eb765
Asfandiyar <asfandiartleuken8@gmail.com>
2026-01-31 22:29:57 +0500

    Add if else examples in Python

---
+++ pp2/Practice2/if_else/if_else.py
@@ -0,0 +1,35 @@
+# 1) Basic if / else example
+x = 10
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is 5 or less")
+
+# 2) Check age condition
+age = 17
+if age >= 18:
+    print("Access granted")
+else:
+    print("Access denied")
+
+# 3) Even or odd number check
+number = 8
+if number % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
+# 4) Compare two numbers
+a = 5
+b = 9
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than or equal to a")
+
+# 5) Boolean variable with if / else
+is_logged_in = False
+if is_logged_in:
+    print("User is logged in")
+else:
+    print("User is not logged in")
