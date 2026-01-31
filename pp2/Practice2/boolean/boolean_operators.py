2925f3db963bd72a560fc91a099b5c1e18d130d4
Asfandiyar <asfandiartleuken8@gmail.com>
2026-01-31 22:25:41 +0500

    Add boolean operators examples in Python

---
+++ pp2/Practice2/boolean/boolean_operators.py
@@ -0,0 +1,23 @@
+# 1) and операторы
+a = 10
+b = 5
+print(a > 5 and b < 10)
+
+# 2) or операторы
+x = 3
+y = 7
+print(x > 5 or y > 5)
+
+# 3) not операторы
+is_student = True
+print(not is_student)
+
+# 4) and + or бірге қолдану
+age = 17
+has_permission = True
+print(age >= 18 or has_permission)
+
+# 5) Күрделі логикалық тексеру
+score = 85
+attendance = True
+print(score >= 80 and attendance)
