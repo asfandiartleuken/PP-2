a200ae72f3f99d6ab24be441a39b38b61ce4909f
Asfandiyar <asfandiartleuken8@gmail.com>
2026-02-27 23:16:29 +0500

    math

---
+++ pp2/Practice4/math.py
@@ -0,0 +1,31 @@
+#1
+import math
+
+degree = float(input("Input degree: "))
+radian = degree * math.pi/180
+
+print("Output radian:", round(radian, 6))
+#2
+height = float(input("Height: "))
+base1 = float(input("Base, first value: "))
+base2 = float(input("Base, second value: "))
+
+area = (base1 + base2) / 2 * height
+
+print("Expected Output:", area)
+#3
+import math
+
+n = int(input("Input number of sides: "))
+a = float(input("Input the length of a side: "))
+
+area = (n * a * a) / (4 * math.tan(math.pi / n))
+
+print("The area of the polygon is:", round(area))
+#4
+base = float(input("Length of base: "))
+height = float(input("Height of parallelogram: "))
+
+area = base * height
+
+print("Expected Output:", area)
